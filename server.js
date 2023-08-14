845b594c46dd998cf8a65ba27edd1f24336734b1
Mattlotus <mattlotus91@gmail.com>
2023-08-14 10:53:46 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express')
+const app = express()
+PORT= 3000
+
+
+const Pokemon = require('./models/pokemon.js');
+
+// app.set('view engine', 'jsx')
+// app.engine('jsx', require("express-react-views").createEngine())
+
+app.get("/" , (req,res)=>{
+    res.send('<h1>Welcome to the Pokemon App</h1>')
+    
+})
+app.get ('/pokemon' , (req,res)=>{
+    res.send(Pokemon)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, (req,res)=>{
+    console.log('Listening to 3K RADIO')
+});
